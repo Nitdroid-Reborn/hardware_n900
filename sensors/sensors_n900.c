64341402e24220364ada43a3a7673a15c7732732
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2010-12-26 23:20:38 +0300

    Accelerometer HAL module, Gingerbread-ready

--- sensors/sensors_n900.c
+++ sensors/sensors_n900.c
@@ -3,7 +3,6 @@
  * Copyright (C) 2010 Nitdroid Project
  *
  * Author: Alexey Roslyakov <alexey.roslyakov@newsycat.com>
- * Based on module for OpenMoko by Michael Trimarchi 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,26 +43,16 @@
 #include <cutils/properties.h>
 
 /******************************************************************************/
+
+#define SENSORS_N900_DEBUG 0
+
 #define ID_BASE SENSORS_HANDLE_BASE
 #define ID_ACCELERATION (ID_BASE+0)
 
-#define EVENT_MASK_ACCEL_ALL    ( (1 << ABS_X) | (1 << ABS_Y) | (1 << ABS_Z))
 #define DEFAULT_THRESHOLD 100
-
-#define SEC_TO_NSEC 1000000000LL
-#define USEC_TO_NSEC 1000
-#define CONTROL_READ 0
-#define CONTROL_WRITE 1
-#define WAKE_SOURCE 0x1a
-
 #define SYSFS_PATH "/sys/class/i2c-adapter/i2c-3/3-001d/"
 
-uint32_t active_sensors;
-int sensor_fd = -1;
-int event_fd = -1;
-int control_fd[2] = { -1, -1 };
-sensors_data_t sensors;
-
+static int sensor_fd = -1;
 
 static int
 write_string(char const *file, const char const *value)
@@ -127,197 +116,93 @@ static int sensors_get_list(struct sensors_module_t *module,
     return 1;
 }
 
+static int sensors_set_delay_n900(struct sensors_poll_device_t *dev, int handle, int64_t ns)
+{
+    LOGD("Control set delay %lld ns is not supported and fix to 200 ms\n", ns);
+    return 0;
+}
+
 /** Close the sensors device */
-static int
-close_sensors(struct hw_device_t *dev)
-{
-#if 1
-    struct sensors_data_device_t *device_data =
-                    (struct sensors_data_device_t *)dev;
-    if (device_data) {
-        if (event_fd > 0)
-            close(event_fd);
-        free(device_data);
-    }
-#endif
-    return 0;
-}
-
-static native_handle_t *control_open_data_source(struct sensors_control_device_t *dev)
-{
-    native_handle_t *hd;
-
-#if 0
-    if (event_fd != -1) {
-        LOGE("Sensor open and not yet closed\n");
-        return NULL;
-    }
-#endif
-
-    if (control_fd[0] == -1 && control_fd[1] == -1) {
-        if (socketpair(AF_LOCAL, SOCK_STREAM, 0, control_fd) < 0 )
-        {
-            LOGE("could not create thread control socket pair: %s",
-                 strerror(errno));
-            return NULL;
-        }
-    }
-
-    LOGD("%s sensor_fd=%d\n", __func__, sensor_fd);
-    hd = native_handle_create(1, 0);
-    hd->data[0] = control_fd[0];
-
-    return hd;
-}
-
-static int control_activate(struct sensors_control_device_t *dev,
-                            int handle, int enabled)
-{
-    uint32_t mask = (1 << handle);
-    uint32_t sensors;
-    uint32_t new_sensors, active, changed;
-
-    sensors = enabled ? mask : 0;
-    active = active_sensors;
-    new_sensors = (active & ~mask) | (sensors & mask);
-    changed = active ^ new_sensors;
-    if (!changed)
-        return 0;
-
-    active_sensors = new_sensors;
-
-    if (!enabled)
-        LOGD("Deactivate sensor\n");
-    else
-        LOGD("Activate sensor\n");
-
-    return 0;
-}
-
-static int control_set_delay(struct sensors_control_device_t *dev, int32_t ms)
-{
-    LOGD("Control set delay %d ms is not supported and fix to 400\n", ms);
-    return 0;
-}
-
-static int control_wake(struct sensors_control_device_t *dev)
-{
-    LOGD("Control wake\n");
-    char ch = WAKE_SOURCE;
-    write(control_fd[CONTROL_WRITE], &ch, sizeof(char));
-    return 0;
-}
-
-static int control_close(struct hw_device_t *dev)
+static int sensors_close_n900(struct hw_device_t *dev)
 {
     struct sensors_control_device_t *device_control = (void *) dev;
-    close(control_fd[CONTROL_WRITE]);
-    close(control_fd[CONTROL_READ]);
-    close(sensor_fd);
-    sensor_fd = -1; control_fd[0] = -1; control_fd[1] = -1;
+    LOGD("%s\n", __func__);
+    if (sensor_fd > 0) {
+        close(sensor_fd);
+        sensor_fd = -1;
+    }
     free(device_control);
     return 0;
 }
 
-static int sensors_open(struct sensors_data_device_t *dev, native_handle_t* hd)
+static int sensors_activate_n900(struct sensors_poll_device_t *dev, int handle, int enabled)
 {
-    event_fd = open(SYSFS_PATH "coord", O_RDONLY | O_NONBLOCK);
-    if (event_fd < 0) {
-        LOGE("coord open failed in %s: %s", __FUNCTION__, strerror(errno));
-        return -1;
+    if (1 /*enabled*/) {
+        if (sensor_fd < 0) {
+            sensor_fd = open(SYSFS_PATH "coord", O_RDONLY | O_NONBLOCK);
+            if (sensor_fd < 0) {
+                LOGE("coord open failed in %s: %s", __FUNCTION__, strerror(errno));
+                return -1;
+            }
+        }
     }
 
-    sensors.vector.status = SENSOR_STATUS_ACCURACY_HIGH;
     LOGD("%s\n", __func__);
     write_int("ths", DEFAULT_THRESHOLD);
-    native_handle_close(hd);
-    native_handle_delete(hd);
 
     return 0;
 }
 
-static int sensors_close(struct sensors_data_device_t *dev)
-{
-    if (event_fd > 0) {
-        close(event_fd);
-        event_fd = -1;
-        LOGD("%s\n", __func__);
-    }
-    return 0;
-}
-
-static int sensors_poll(struct sensors_data_device_t *dev, sensors_data_t* values)
+static int sensors_poll_n900(struct sensors_poll_device_t *dev, sensors_event_t* event, int count)
 {
     fd_set rfds;
     char coord[20];
-    int ret;
-    uint32_t new_sensors = 0;
-    int fd, select_dim;
-	struct timeval timeout;
+    int ret, fd;
+    struct timeval timeout;
 
-    fd = event_fd;
-	if (fd < 1) {
-		LOGE("Bad coord file descriptor: %d", fd);
-		return -1;
-	}
-
-#if 0
-    select_dim = (fd > control_fd[CONTROL_READ]) ?
-                 fd + 1 : control_fd[CONTROL_READ] + 1;
-#else
-	select_dim = fd + 1;
-#endif
-
-    while (1)
-    {
-        FD_ZERO(&rfds);
-        FD_SET(fd, &rfds);
-        //FD_SET(control_fd[CONTROL_READ], &rfds);
-
-        do {
-			timeout.tv_sec = 0;
-			timeout.tv_usec = 100000;
-            ret = select(select_dim, &rfds, NULL, NULL, &timeout);
-        } while (ret < 0 && errno == EINTR);
-
-		if (ret < 0) {
-			LOGE("%s select error: %d", __func__, errno);
-			return ret;
-		}
-
-#if 0
-        if (FD_ISSET(control_fd[CONTROL_READ], &rfds))
-        {
-            char ch;
-            read(control_fd[CONTROL_READ], &ch, sizeof(ch));
-            LOGD("Wake up by the control system\n");
-            return -EWOULDBLOCK;
-        }
-#endif
-
-        lseek(fd, 0, SEEK_SET);
-        ret = read(fd, coord, sizeof(coord));
-        if (ret < 0)
-            break;
-
-        FD_CLR(control_fd[CONTROL_READ], &rfds);
-
-		float x = 0, y = 0, z = 0;
-		sscanf(coord, "%f %f %f\n", &x, &y, &z);
-        sensors.acceleration.x = (-GRAVITY_EARTH * y) / 1000;
-        sensors.acceleration.y = (-GRAVITY_EARTH * x) / 1000;
-        sensors.acceleration.z = (-GRAVITY_EARTH * z) / 1000;
-        sensors.time = 0;
-#if 0
-        LOGD("%s: sensor event %f, %f, %f\n", __FUNCTION__,
-             sensors.acceleration.x, sensors.acceleration.y,
-             sensors.acceleration.z);
-#endif
-        *values = sensors;
-        values->sensor = ID_ACCELERATION;
-        return ID_ACCELERATION;
+    fd = sensor_fd;
+    if (fd < 1) {
+        LOGE("Bad coord file descriptor: %d", fd);
+        return -1;
     }
-    return 0;
+
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+
+    do {
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 1000000;
+        ret = select(fd + 1, &rfds, NULL, NULL, &timeout);
+    } while (ret < 0 && errno == EINTR);
+
+    if (ret < 0) {
+        LOGE("%s select error: %s", __func__, strerror(errno));
+        return -errno;
+    }
+
+    lseek(fd, 0, SEEK_SET);
+    ret = read(fd, coord, sizeof(coord));
+    if (ret < 0) {
+        LOGE("%s read error: %s", __func__, strerror(errno));
+        return -errno;
+    }
+
+    float x = 0, y = 0, z = 0;
+    sscanf(coord, "%f %f %f\n", &x, &y, &z);
+    event->acceleration.x = (-GRAVITY_EARTH * y) / 1000;
+    event->acceleration.y = (-GRAVITY_EARTH * x) / 1000;
+    event->acceleration.z = (-GRAVITY_EARTH * z) / 1000;
+    event->timestamp = 0;
+    event->version = sizeof(struct sensors_event_t);
+    event->type = ID_ACCELERATION;
+
+#if (SENSORS_N900_DEBUG > 0)
+    LOGD("%s: sensor event %f, %f, %f\n", __FUNCTION__,
+         event->acceleration.x, event->acceleration.y,
+         event->acceleration.z);
+#endif
+
+    return 1;
 }
 
 /******************************************************************************/
@@ -332,38 +217,22 @@ static int open_sensors(const struct hw_module_t* module, char const* name,
 {
     int status = -EINVAL;
 
-    if (!strcmp(name, SENSORS_HARDWARE_CONTROL))
-    {
-        struct sensors_control_device_t *device_control =
-                        malloc(sizeof(struct sensors_control_device_t));
-        memset(device_control, 0, sizeof(*device_control));
+    if (!strcmp(name, SENSORS_HARDWARE_POLL)) {
+        struct sensors_poll_device_t *dev =
+            malloc(sizeof(*dev));
+        memset(dev, 0, sizeof(*dev));
 
-        device_control->common.tag = HARDWARE_DEVICE_TAG;
-        device_control->common.version = 0;
-        device_control->common.module = (struct hw_module_t*)module;
-        device_control->common.close = control_close;
-        device_control->open_data_source = control_open_data_source;
-        device_control->activate = control_activate;
-        device_control->set_delay = control_set_delay;
-        device_control->wake = control_wake;
-        sensor_fd = -1;
-        *device = &device_control->common;
+        dev->common.tag = HARDWARE_DEVICE_TAG;
+        dev->common.version = 0;
+        dev->common.module = (struct hw_module_t*)module;
+        dev->common.close = sensors_close_n900;
+        dev->activate = sensors_activate_n900;
+        dev->setDelay = sensors_set_delay_n900;
+        dev->poll = sensors_poll_n900;
+        
+        *device = &dev->common;
         status = 0;
-     } else if (!strcmp(name, SENSORS_HARDWARE_DATA)) {
-        struct sensors_data_device_t *device_data =
-                        malloc(sizeof(struct sensors_data_device_t));
-        memset(device_data, 0, sizeof(*device_data));
-
-        device_data->common.tag = HARDWARE_DEVICE_TAG;
-        device_data->common.version = 0;
-        device_data->common.module = (struct hw_module_t*)module;
-        device_data->common.close = close_sensors;
-        device_data->data_open = sensors_open;
-        device_data->data_close = sensors_close;
-        device_data->poll = sensors_poll;
-        event_fd = -1;
-        *device = &device_data->common;
-        status = 0;
+        fprintf(stderr, "!");
     }
     return status;
 }
@@ -380,7 +249,7 @@ struct sensors_module_t HAL_MODULE_INFO_SYM =
 {
     .common = {
         .tag           = HARDWARE_MODULE_TAG,
-        .version_major = 1,
+        .version_major = 2,
         .version_minor = 0,
         .id            = SENSORS_HARDWARE_MODULE_ID,
         .name          = "N900 sensors module",
@@ -389,4 +258,3 @@ struct sensors_module_t HAL_MODULE_INFO_SYM =
     },
     .get_sensors_list = sensors_get_list
 };
-
